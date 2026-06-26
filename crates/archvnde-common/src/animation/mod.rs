84b42802f043ba0b13189c76ebbaf0da3d8b764f
tdkhoa <I4104.vn@gmail.com>
2026-06-26 10:13:38 +0700

    refactor: clean up unused fade animation module

--- crates/archvnde-common/src/animation/mod.rs
+++ crates/archvnde-common/src/animation/mod.rs
@@ -1,12 +1,10 @@
 pub mod easing;
 pub mod slide;
-pub mod fade;
 pub mod genie;
 pub mod css_genie;
 pub mod island;
 
 pub use slide::{slide_in, slide_out, slide_out_cb, SlideDirection};
-pub use fade::{fade_in, fade_out_cb};
 pub use genie::{genie_in, genie_out};
 pub use css_genie::{css_genie_in, css_genie_out};
 pub use island::{island_zoom_in, island_zoom_out};
