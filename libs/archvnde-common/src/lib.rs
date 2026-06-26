00c7ba17c27439eb05fca0adb2b3e6aa5a6d816b
tdkhoa <I4104.vn@gmail.com>
2026-06-26 14:43:43 +0700

    fix(common): re-export desktop module to restore public API paths

--- libs/archvnde-common/src/lib.rs
+++ libs/archvnde-common/src/lib.rs
@@ -1,14 +1,15 @@
-pub mod config;
+pub mod core;
 pub mod theme;
 pub mod animation;
 pub mod icon;
 pub mod island;
 pub mod models;
-pub mod desktop;
 pub mod window;
 
-pub use models::{ThemeConfig, ShellConfig, IslandState, PinnedApp, DockConfig};
-pub use config::{get_archvnde_config_dir, get_dock_config_path, load_dock_config, save_dock_config};
+pub use models::{ThemeConfig, ShellConfig, IslandState};
+pub use core::config::get_archvnde_config_dir;
 pub use theme::init_theme;
 pub use island::{update_island_state, clear_island_state, get_island_state_path};
-pub use desktop::{find_desktop_apps, DesktopApp};
+pub use core::desktop::{find_desktop_apps, DesktopApp};
+pub use core::desktop;
+pub use window::init_layer_window;
