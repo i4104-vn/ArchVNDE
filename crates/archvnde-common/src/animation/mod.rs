3d13f24c0033d614d7b7fb1920db572fb367c93f
tdkhoa <I4104.vn@gmail.com>
2026-06-26 08:57:57 +0700

    fix: change island notification animations to in-place fade in/out to keep po...

--- crates/archvnde-common/src/animation/mod.rs
+++ crates/archvnde-common/src/animation/mod.rs
@@ -4,7 +4,7 @@ pub mod genie;
 pub mod css_genie;
 pub mod island;
 
-pub use slide::{slide_in, slide_out, slide_out_cb, SlideDirection};
+pub use slide::{slide_in, slide_out, slide_out_cb, SlideDirection, fade_in, fade_out_cb};
 pub use genie::{genie_in, genie_out};
 pub use css_genie::{css_genie_in, css_genie_out};
 pub use island::{island_zoom_in, island_zoom_out};
